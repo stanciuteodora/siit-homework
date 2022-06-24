508a895275ab5200d8049b91e87abf1927db5ab7
teodorastanciu <teodora_balan@yahoo.com>
2022-06-24 18:15:34 +0300

    Aceasta este a doua tema

---
+++ src/main/java/siit/homework02/PrettyPrinter.java
@@ -0,0 +1,20 @@
+package siit.homework02;
+
+/**
+ * Pretty prints stuff.
+ */
+public class PrettyPrinter {
+    /**
+     * Displays Sales Representatives and a message.
+     *
+     * @param representatives       a list of {@link SalesRepresentative}.
+     * @param beforePrintingMessage a message to be printed before the list.
+     */
+    public void display(SalesRepresentative[] representatives, String beforePrintingMessage) {
+        System.out.println(beforePrintingMessage);
+        for (int i = 0; i < representatives.length; i++) {
+            System.out.println(representatives[i]);
+
+        }
+    }
+}
